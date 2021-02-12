d14d8c72069ba62c1a163b609dbda31c16a5b811
luz adriana <luzadri.2908@gmail.com>
2021-02-11 23:48:50 -0500

    Add Style

--- source.js
+++ source.js
@@ -11,7 +11,8 @@ const getReqParams = (r) => {
 const queryLocations = async () => {
   return await query({
     sql: `select l.id as locationId, l.name as locationName, l.capacity as locationCapacity, count(zl.location_id) as totalLocationZombies from location l
-    left join zombie_location zl on zl.location_id = l.id and zl.is_in = true group by zl.location_id`,
+    left join zombie_location zl on zl.location_id = l.id and zl.is_in = true 
+    group by l.id order by l.name ASC`,
   });
 };
 
@@ -40,7 +41,7 @@ const getZombiesLocation = async (req, res) => {
     sql: `select zl.zombie_id, z.name as zombie_name, z.image as zombie_image, zl.location_id, l.name as location_name, zl.date from zombie_location zl
     INNER JOIN location l on zl.location_id = l.id
     INNER JOIN zombie z on zl.zombie_id = z.id
-    where zl.is_in = true`,
+    where zl.is_in = true order by l.name ASC`,
   });
   const locations = await queryLocations();
   const assignedZombies = [];
@@ -74,25 +75,31 @@ const getZombiesLocation = async (req, res) => {
 
 const updateZombieLocation = async (req, res) => {
   const { zombieId, locationId } = getReqParams(req);
-  const clearLocation = await query({
-    sql: `Update zombie_location set is_in = false where zombie_id = ?`,
-    params: [zombieId],
-  });
-  if (clearLocation) {
-    const r = await query({
-      sql: `INSERT INTO zombie_location (location_id, zombie_id, date, is_in) VALUES (?, ?, ?, true)`,
-      params: [locationId, zombieId, new Date().toISOString()],
+
+  if(zombieId && locationId){
+    const clearLocation = await query({
+      sql: `Update zombie_location set is_in = false where zombie_id = ?`,
+      params: [zombieId],
     });
-    if (r) {
-      res.status(200);
-      res.json([]);
+    if (clearLocation) {
+      const r = await query({
+        sql: `INSERT INTO zombie_location (location_id, zombie_id, date, is_in) VALUES (?, ?, ?, true)`,
+        params: [locationId, zombieId, new Date().toISOString()],
+      });
+      if (r) {
+        res.status(200);
+        res.json([]);
+      } else {
+        res.status(400);
+        res.json([]);
+      }
     } else {
       res.status(400);
       res.json([]);
     }
-  } else {
+  }else{
     res.status(400);
-    res.json([]);
+    res.json();
   }
 };
 
